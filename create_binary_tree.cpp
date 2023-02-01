def756213be5a18f2b9e5039294fb48f79a041df
saziye <96542141+szydag@users.noreply.github.com>
2023-02-01 17:25:00 +0300

    Add files via upload

---
+++ create_binary_tree.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Agac
+{
+	int sayi;
+	Agac*sol;
+	Agac*sag;
+}Agac;
+
+Agac*Ekle(Agac*dugum,int sayi)
+{
+	if(dugum==NULL)
+	{
+		Agac*kok=(Agac*)malloc(sizeof(Agac));
+		kok->sayi=sayi;
+		kok->sag=NULL;
+		kok->sol=NULL;
+		return kok;
+	}
+	if( sayi < dugum->sayi)
+	{
+		dugum->sol=Ekle(dugum->sol,sayi);
+		return dugum;
+	}
+	if(sayi > dugum->sayi)
+	{
+		dugum->sag=Ekle(dugum->sag,sayi);
+		return dugum;
+	}
+}
+
+Listele(Agac*dugum)
+{
+	if(dugum==NULL)
+	{
+		return 0;
+	}
+	printf(" %d ",dugum->sayi);
+	Listele(dugum->sag);
+	Listele(dugum->sol);
+}
+
+
+ main()
+{
+	Agac*kok=NULL;
+	
+	for(int i=0;i<10;i++)
+	{
+		int sayi=rand()%100;
+		if(kok==NULL)
+		{
+			kok=Ekle(kok,sayi);
+		}
+		else
+		{
+	    	Ekle(kok,sayi);
+		}
+	}
+	printf("Olusturulan Agac ");
+	Listele(kok);
+}
